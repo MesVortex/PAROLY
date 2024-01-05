cf80f9e0064a547e1acceb1a56da8a981f82c962
unknown <ismail.ouali@ump.ac.ma>
2024-01-05 12:14:32 +0100

    modify controller

--- app/controllers/PlaylistController.php
+++ app/controllers/PlaylistController.php
@@ -1,5 +1,17 @@
 <?php
 
-class PlaylistController extends {
+class PlaylistController extends controller{
+    private $playlistname;
+
+    public function __construct(){
+
+    }
+
+    public function showplaylist(){
+
+        $arr = array()
+    }
+
+    public function 
 
 }
